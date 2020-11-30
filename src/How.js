5113c5614552df408612a9affc5639c7c3721364
unknown <jenearly@gmail.com>
2020-11-30 10:06:23 -0500

    style fixes and link fixes

--- src/How.js
+++ src/How.js
@@ -17,26 +17,59 @@ export default function ({ context }) {
 
   return (
     <div className="how-big-container">
+<span className="how-notice">
+  <p>You can go ahead and get started by using the <strong>Methods</strong> on the right</p>
+  <p>Below are some videos on how to use this Monday's View App</p>
+  <p>Visit our <a href="https://itemvisualizer.com/">website</a> for more information.</p>
+</span>
       <div className="how-container">
         {context.play === 0 ? (
           <div className="one">
             <h3>Welcome</h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/9CP0eiwuVIc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/9CP0eiwuVIc"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         ) : context.play === 1 ? (
           <div className="two">
-            <h3>Show the website</h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/b_IXUVyQL4o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h3>Show the Website</h3>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/b_IXUVyQL4o"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         ) : context.play === 2 ? (
           <div className="three">
             <h3>Show the Idea</h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/q5DJc0kNPcs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/q5DJc0kNPcs"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         ) : context.play === 3 ? (
           <div className="four">
             <h3>Show the Video</h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/LalyTRtJuWo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/LalyTRtJuWo"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         ) : null}
         <div className="prev" onClick={(e) => prev()}>
